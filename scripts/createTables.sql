9059d04f245e478cd02189adf52976892672f5be
Márkus Bence <markus.bence@konasoft.hu>
2021-12-06 13:52:50 +0100

    sql create scripts

---
+++ scripts/createTables.sql
@@ -0,0 +1,40 @@
+USE `todo_db`;
+
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(64) NOT NULL,
+    `first_name` VARCHAR(32) NOT NULL,
+    `last_name` VARCHAR(32) NOT NULL,
+    `password` VARCHAR(512) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE `lists` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(64) NOT NULL,
+    `description` VARCHAR(256) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `color` VARCHAR(6) NOT NULL DEFAULT 'ffffff',
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE `todos` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(64) NOT NULL,
+    `description` VARCHAR(256) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `list_id` INTEGER NULL,
+    `completed_fl` BOOLEAN NOT NULL DEFAULT false,
+    `due_date` DATETIME(3) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
