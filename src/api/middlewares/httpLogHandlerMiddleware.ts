0bdf7522fe5ede19b3d083cac1fcd038231b13d8
Márkus Bence <markus.bence@konasoft.hu>
2021-12-08 15:21:35 +0100

    fix for my ocd

--- src/api/middlewares/httpLogHandlerMiddleware.ts
+++ src/api/middlewares/httpLogHandlerMiddleware.ts
@@ -1,7 +1,7 @@
 'use strict';
 
-import morgan, { StreamOptions } from 'morgan';
 import logger from '../../utils/logger';
+import morgan, { StreamOptions } from 'morgan';
 
 const logStream: StreamOptions = { write: (message) => logger.info(message.slice(0, message.length - 1)) };
 const httpLogHandlerMiddleware = morgan(':remote-addr :method :url HTTP/:http-version :status - :response-time ms', {
