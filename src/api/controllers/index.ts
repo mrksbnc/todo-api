0edddf8e1ae29f207ed0c27e9408b665bd058d87
Markus Bence <markus.bence@icloud.com>
2021-12-09 19:12:18 +0100

    list controller added to index

--- src/api/controllers/index.ts
+++ src/api/controllers/index.ts
@@ -2,8 +2,13 @@
 
 import services from '../../services';
 import AuthController from './authController';
+import ListController from './listController';
 import UserController from './userController';
 
-const controllers = [new UserController(services.user), new AuthController(services.user, services.auth)];
+const controllers = [
+  new UserController(services.user),
+  new ListController(services.list),
+  new AuthController(services.user, services.auth),
+];
 
 export default controllers;
